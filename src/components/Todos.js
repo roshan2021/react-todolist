98386ff1acc36cd8f9beb779b34903301a0aa35c
Roshan Jacob <roshan.jacob2021@gmail.com>
2020-08-01 20:22:47 -0500

    created key for map todo

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -3,8 +3,10 @@ import TodoItem from "./TodoItem";
 
 class Todos extends Component {
     render() {
+        //when map through something it is creating a list, list should have keys
+        //add keys, unique ie. id
         return this.props.todos.map((todo) => (
-           <TodoItem todo={todo}/>
+           <TodoItem key={todo.id} todo={todo}/>
         ));
     }
 }
